4b37574c2d0d9a27b2caca187714feda0facefef
kiitosu <kh0412@gmail.com>
2025-06-03 21:16:28 +0900

    フロントエンドの実装

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,7 +5,9 @@ import { createClient } from "@connectrpc/connect";
 import { createConnectTransport } from "@connectrpc/connect-web";
 
 // 接続したいサービスをインポート
-import { GreetService } from "../gen/greet/v1/greet_pb";
+import { GreetService, TodoService } from "../gen/greet/v1/greet_pb";
+import type { Todo } from "../gen/entpb/entpb_pb"
+
 
 // transportではどのタイプのエンドポイントを使うか定義します
 // 今回はConnect endpointを使います。
@@ -16,10 +18,12 @@ const transport = createConnectTransport({
 
 // サービス定義とtransportを組み合わせてクライアントを作ります
 const client = createClient(GreetService, transport)
+const todoClient = createClient(TodoService, transport)
 
 function App() {
     const[requestState, setRequest] = useState("")
     const [responseState, setResponse] = useState<string[]>([]);
+    const [todos, setTodos] = useState<Todo[]>([]);
     return <>
         <form onSubmit={async (e) => {
             e.preventDefault(); // ページリロードを避ける
@@ -39,6 +43,23 @@ function App() {
                 ))
             }
         </>
+
+        <button onClick={async () => {
+            const response = await todoClient.listTodos({})
+            setTodos(response.todos)
+        }}>
+            get todos
+        </button>
+
+        <div>
+            {
+                todos.map( todo => (
+                    <li key={todo.id}>
+                        ID: {todo.id}, タイトル: {todo.text}, 状態: {todo.status}, 優先度: {todo.priority}
+                    </li>
+                ))
+            }
+        </div>
     </>
 }
 export default App
